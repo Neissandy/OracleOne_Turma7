0cab6fd38131d87aba4e38898bbf94ee8a24ffe5
Neissandy <neisandy.silva@gmail.com>
2024-07-02 11:02:12 -0300

    Versão 2

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -23,4 +23,46 @@ if (idade3 >= 18){
 */
 
 
-//------------------------- AULA 2 (EXERCÍCIO)
+//------------------------- AULA 2 (EXERCÍCIO) ---------------------
+
+//QUESTÃO 1 
+/*let diaSemana = prompt('Qual o dia da semana? ');
+
+if (diaSemana == 'sábado' || diaSemana == 'domingo'){
+    alert('Bom final de semana!')
+}else{
+    alert('Boa semana!')
+}*/
+//===========================================================
+
+//QUESTÃO 2
+/*let numero = prompt('Digite um número: ');
+
+if (numero > 0){
+    alert('Esse número é positivo! ');
+}else{2
+    alert('Esse número é negativo!');
+}*/
+//===========================================================
+
+//QUESTÃO 3
+/*let pontos = prompt('Digite os pontos alcançados:');
+
+if(pontos >= 100){
+    alert('Parabéns, você venceu!')
+}else{
+    alert('Tente novamente para ganhar.')
+}*/
+//===========================================================
+
+//QUESTÃO 4
+/*let saldo = prompt('Digite o saldo: ');
+
+alert(`O saldo disponível é ${saldo} reais.`); */
+//===========================================================
+
+//QUESTÃO 5
+/*let nome = prompt('Digite seu nome: ');
+
+alert(`Boas-vindas ${nome}!`); */
+//===========================================================
