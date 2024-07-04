4b95bbccb1182a6d09822a1dd77a2013d6d2af43
Neissandy <neisandy.silva@gmail.com>
2024-07-04 10:42:40 -0300

    Versão 2

--- logica-js-projeto_inicial/aula2.js
+++ logica-js-projeto_inicial/aula2.js
@@ -1,28 +1,4 @@
 
-//----------------------- AULA 1 (EXERCÍCIO) ------------------------------
-
-/* alert("Boas vindas ao nosso site!");
-
-let nome ="Lua";
-let idade = 25;
-let numeroDeVendas = 50;
-let saldoDisponivel = 1000;
-
-alert("Erro! Preencha todos os campos");
-
-let mensagemDeErro = "Erro! Preencha todos os campos";
-alert(mensagemDeErro);
-
-let nome2 = prompt("Qual o seu nome? ");
-let idade2 = prompt("Qual a sua idade? ");
-
-let idade3 = prompt("Digite a sua idade: ")
-if (idade3 >= 18){
-    alert("Pode tirar a habilitação!");
-}
-*/
-
-
 //------------------------- AULA 2 (EXERCÍCIO) ---------------------
 
 //QUESTÃO 1 
